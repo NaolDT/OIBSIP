716dca6f905fe8346a43f985e86edc1212d00a27
naol <naoldera93@gmail.com>
2026-07-21 11:45:09 +0300

    fix: using resend for email

--- WebDev-L3-PizzaDeliveryApp/server/utils/sendEmail.js
+++ WebDev-L3-PizzaDeliveryApp/server/utils/sendEmail.js
@@ -1,16 +1,15 @@
-
 export const sendEmail = async ({ to, subject, html }) => {
-  const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+  const response = await fetch("https://api.resend.com/emails", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "api-key": process.env.BREVO_API_KEY,
+      Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
     },
     body: JSON.stringify({
-      sender: { name: "Pizza Delivery App", email: process.env.EMAIL_USER },
-      to: [{ email: to }],
+      from: "Pizza Delivery App <onboarding@resend.dev>",
+      to: [to],
       subject,
-      htmlContent: html,
+      html,
     }),
   });
 
